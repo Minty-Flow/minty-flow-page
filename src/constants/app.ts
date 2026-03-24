8e354b92c78e0b6d0bae75644d7d270f3aae7d77
adelefaell <adelefaell@gmail.com>
2026-03-24 20:55:21 +0200

    app data update

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,14 +1,15 @@
 export const APP = {
-  name: 'Minty Flow',
-  tagline: 'Free, open-source expense tracker.',
-  siteUrl: 'https://flow-mn.github.io/minty-flow-page',
+  name: "Minty Flow",
+  tagline: "Free, open-source expense tracker.",
+  siteUrl: "https://minty-flow.github.io/minty-flow-page",
   contact: {
-    email: 'adelefaell@gmail.com',
+    email: "adelefaell@gmail.com",
   },
   links: {
-    googlePlay: 'https://play.google.com/store/apps/details?id=mn.gege.flow',
-    githubOrg: 'https://github.com/flow-mn',
-    githubReleases: 'https://github.com/flow-mn/minty-flow/releases',
-    githubIssues: 'https://github.com/flow-mn/minty-flow/issues',
+    googlePlay:
+      "https://play.google.com/store/apps/details?id=com.mintyflow.tracker",
+    githubOrg: "https://github.com/Minty-Flow",
+    githubReleases: "https://github.com/Minty-Flow/minty-flow-page/releases",
+    githubIssues: "https://github.com/Minty-Flow/minty-flow/issues",
   },
-} as const
+} as const;
